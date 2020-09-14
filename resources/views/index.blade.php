4f825db399d691fc216dddad26f43dad4994d92e
Vinícius Santana <52092114+VinniVicius@users.noreply.github.com>
2020-09-14 00:07:30 -0300

    Primeira Dobra
    Envio da primeira dobra do layout, não finalizado.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,4 +1,12 @@
 @extends('templates.main')
 @section('content')
-Qualquer coisa
+<section class="jumbotron jumbotron-fluid jumbo-esports">
+  <article class="container">
+    <h1 class="display-4">eSports e Saúde</h1>
+    <article style="position: relative;">
+        <button type="button" class="btn btn-dark btn-lg rounded-0" style="color: #343a40"></button>
+        <button type="button" class="btn btn-dark btn-lg rounded-0">Saiba mais</button>
+    </article>
+  </article>
+</section>
 @endsection
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -5,9 +5,69 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="{{url('https://fonts.googleapis.com/icon?family=Material+Icons')}}">
     <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
 </head>
+<nav class="navbar navbar-expand-lg navbar-eSports bg-eSports">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" loading="lazy">
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+  <span class="material-icons">menu</span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+    <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuEsports" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          eSports
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLinkEsports">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLesoes" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Lesões
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLesoes">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuRecomenda" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Recomendações
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuRecomenda">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuTeste" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Teste
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuTeste">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+    <span class="navbar-text">
+      <a href="#">Login / Cadastrar</a>
+    </span>
+</nav>
 <body>
     @yield('content')
 </body>
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 </html>
