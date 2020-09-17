7feccd13c751f388e2babb64564fcba8eea478a7
Vinícius Santana <52092114+VinniVicius@users.noreply.github.com>
2020-09-16 23:51:21 -0300

    Minor link fix for Login/Register
    Linkagem da âncora de Login e Registrar-se corrigida.

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -60,7 +60,41 @@
       </li>
     </ul>
     <span class="navbar-text">
-      <a href="#">Login / Cadastrar</a>
+      <!-- Right Side Of Navbar -->
+      <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            <li class="nav-item">
+                                <div class="nav-link text-white"> / </div>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar-se') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
     </span>
 </nav>
 <body>
