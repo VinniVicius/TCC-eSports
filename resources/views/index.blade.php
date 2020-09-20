43dbdf96babf9dda5c6377372ec8e54d4f0c7a2d
Vinícius Santana <52092114+VinniVicius@users.noreply.github.com>
2020-09-20 13:33:30 -0300

    Top Navbar changes
    Algumas mudanças na barra de navegação do topo.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,20 @@
   <article class="container">
     <h1 class="display-4">eSports e Saúde</h1>
     <article style="position: relative;">
-        <button type="button" class="btn btn-dark btn-lg rounded-0" style="color: #343a40"></button>
-        <button type="button" class="btn btn-dark btn-lg rounded-0">Saiba mais</button>
+      <button type="button" class="btn btn-dark btn-lg rounded-0" style="color: #343a40"></button>
+      <button type="button" class="btn btn-dark btn-lg rounded-0">Saiba mais</button>
     </article>
+  </article>
+</section>
+<section class="container-fluid bg-dobra-2 min-vh-100">
+  <article class="container dobra-2">
+    <div class="col-lg-6">
+      <button type="button" class="btn btn-dark btn-lg rounded-0" style="color: #343a40"></button>
+      <button type="button" class="btn btn-dark btn-lg rounded-0">eSports</button>
+      <h1 class="display-4 text-center">Introdução ao<br> eSports</h1>
+    </div>
+    <div class="w-100"></div>
+    <div class="col"></div>
   </article>
 </section>
 @endsection
--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -1,24 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{url('https://fonts.googleapis.com/icon?family=Material+Icons')}}">
-    <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>TCC - eSports</title>
+  <link rel="stylesheet" href="{{url('https://fonts.googleapis.com/icon?family=Material+Icons')}}">
+  <link rel="stylesheet" href="{{url('assets/bootstrap/css/bootstrap.min.css')}}">
+  <link rel="stylesheet" href="{{url('assets/css/style.css')}}">
 </head>
 <nav class="navbar navbar-expand-lg navbar-eSports bg-eSports">
-  <a class="navbar-brand" href="#">
-    <img src="/docs/4.5/assets/brand/bootstrap-solid.svg" width="30" height="30" alt="" loading="lazy">
+  <a class="navbar-brand" href="{{ url('/') }}">
+    <img class="logo" src="/assets/img/logo-2.png" alt="Logo TCC eSports" loading="lazy">
   </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-  <span class="material-icons">menu</span>
+    <span class="material-icons">menu</span>
   </button>
   <div class="collapse navbar-collapse" id="navbarText">
     <ul class="navbar-nav mr-auto">
-    <li class="nav-item dropdown">
+      <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuEsports" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           eSports
         </a>
@@ -62,46 +63,47 @@
     <span class="navbar-text">
       <!-- Right Side Of Navbar -->
       <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            <li class="nav-item">
-                                <div class="nav-link text-white"> / </div>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar-se') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
-                                </a>
+        <!-- Authentication Links -->
+        @guest
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+        </li>
+        <li class="nav-item">
+          <div class="nav-link text-white"> | </div>
+        </li>
+        @if (Route::has('register'))
+        <li class="nav-item">
+          <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar-se') }}</a>
+        </li>
+        @endif
+        @else
+        <li class="nav-item dropdown">
+          <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+            {{ Auth::user()->name }}
+          </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
+          <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+            <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
+              {{ __('Logout') }}
+            </a>
 
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+              @csrf
+            </form>
+          </div>
+        </li>
+        @endguest
+      </ul>
     </span>
 </nav>
+
 <body>
-    @yield('content')
+  @yield('content')
 </body>
 <!-- JS, Popper.js, and jQuery -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
 </html>
