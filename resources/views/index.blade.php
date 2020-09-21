bfb8b7481f8ed19472196e4e22bec408dd4a472a
MelguiXD <71021497+MelguiXD@users.noreply.github.com>
2020-09-21 01:23:25 -0300

    Css e Gradiente comentado
    btn - Full negro e gradiente comentado

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,14 +5,14 @@
     <h1 class="display-4">eSports e Saúde</h1>
     <article style="position: relative;">
       <button type="button" class="btn btn-dark btn-lg rounded-0" style="color: #343a40"></button>
-      <button type="button" class="btn btn-dark btn-lg rounded-0">Saiba mais</button>
+      <button type="button" class="btn btn-dark btn-lg rounded-0 btn-full-negro">Saiba mais</button>
     </article>
   </article>
 </section>
-<section class="container-fluid bg-dobra-2 min-vh-100">
+<section class="container-fluid bg-dobra-2 min-vh-100" style="border-bottom: 5px solid #0eb7b7">
   <article class="container dobra-2">
     <div class="col-lg-6">
-      <button type="button" class="btn btn-dark btn-lg rounded-0" style="color: #343a40"></button>
+      <button type="button" class="btn btn-dark btn-lg rounded-0 btn-full-negro" style="color: #343a40"></button>
       <button type="button" class="btn btn-dark btn-lg rounded-0">eSports</button>
       <h1 class="display-4 text-center">Introdução ao<br> eSports</h1>
     </div>
